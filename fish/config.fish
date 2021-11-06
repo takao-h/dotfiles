85db0587cb4e6a8521501050ace8642453545dbc
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-11-06 22:58:25 +0900

    added alias

--- fish/config.fish
+++ fish/config.fish
@@ -19,6 +19,33 @@ alias nb 'git checkcout -b'
 ## peco
 # alias ghw='gh repo view -w $(ghq list | peco)'
 
+function peco_cd
+  set selected_dir (ls -1d */ | peco)
+  if [ $selected_dir ]
+    cd $selected_dir
+    commandline -f repaint
+  end
+end
+
+function fish_user_key_bindings
+  bind /cc 'peco_cd' #追記
+end
+
+# ghq + peco
+function ghq_peco_repo
+  set selected_repository (ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_repository" ]
+    cd $selected_repository
+    echo " $selected_repository "
+    commandline -f repaint
+  end
+end
+
+# fish key bindings
+function fish_user_key_bindings
+  bind /cg ghq_peco_repo
+end
+
 ##
 function fish_prompt
   set -l last_status $status
